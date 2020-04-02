3e24e83bede6b719a436770abd4a4d51ec6587a4
Valerie Lui <vallq@users.noreply.github.com>
2020-04-02 18:47:12 +0800

    modify gitignore and package.json to include test env for jest

--- index.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const app = require("./app");
-const PORT = 3001;
-require("./utils/db");
-
-app.listen(PORT, () => {
-  console.log(`Server is now listening on port ${PORT}`);
-});
